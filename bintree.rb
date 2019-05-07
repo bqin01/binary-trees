b630df7682205227f37c263d9b130c3994865332
Bill Z. Qin <bzqin88@gmail.com>
2019-05-07 15:57:26 -0400

    C:/Program Files/Git/create/ functionality, error pages, navigation QOL update

--- bintree.rb
+++ bintree.rb
@@ -3,13 +3,62 @@ require 'sinatra/activerecord'
 require 'yaml/store'
 require './models/node.rb'
 
+set :public_folder, File.dirname(__FILE__)
 set :database_file, 'config/database.yml'
 
 class Appl < Sinatra::Base
-
   get '/' do
     @first_node_id = 0
     erb :index
   end
-
+  post '/create' do
+    continue = true
+    @new_node_name = params['child_name']
+    if @new_node_name == ""
+      @new_node_name = "Unnamed Node"
+    end
+    @old_node = Node.find_by(node_id: params[:oldnode])
+    if params[:dir] == 'left'
+      continue = (@old_node.left == nil)
+    else
+      continue = (@old_node.right == nil)
+    end
+    if continue != true
+      erb :createerror
+    else
+      @new_node_id = 0
+      while Node.find_by(node_id: @new_node_id) != nil
+        @new_node_id = @new_node_id + 1
+      end
+      @the_new_node = Node.create(node_id: @new_node_id, content: @new_node_name)
+      @the_new_node.parent = params[:oldnode]
+      @the_new_node.save
+      if params[:dir] == 'left'
+        @old_node.left = @new_node_id
+      else
+        @old_node.right = @new_node_id
+      end
+      @old_node.save
+      erb :create
+    end
+  end
+  get '/node/:nodeid' do
+    @first_node_id = params[:nodeid]
+    if Node.find_by(node_id: @first_node_id) == nil
+      erb :node404
+    else
+      erb :index
+    end
+  end
+  get '/new/:dir' do
+    @dir = params[:dir]
+    @nodeid = params[:nodeid]
+    if @dir != 'right'
+      @dir = 'left'
+    end
+    erb :new
+  end
+  get '/:anythingelse' do
+    erb :error404
+  end
 end
